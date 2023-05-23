3db221a6e508730f90afd4bc44b877eb420e7d6e
harrissonoliveira <harrissonoliveira@lampp-it.com.br>
2023-05-23 11:12:58 -0300

    Commit Inicial

---
+++ src/main/java/br/com/sistudo/vendas/sistudovendas/SistudoVendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.sistudo.vendas.sistudovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistudoVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistudoVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sistudo/vendas/sistudovendas/SistudoVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistudo.vendas.sistudovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistudoVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
